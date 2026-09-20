5c530fca8bc356dc4cdf8ac813ddd9a816f52a54
Anastasia <anastasiagubareva28@gmail.com>
2026-09-20 22:54:28 +0200

    idea menu + creació de classes

---
+++ programaPrincipal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ProgramaPrincipal {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ProgramaPrincipal s = new ProgramaPrincipal();
+        s.start();
+    }
+
+    public void start() {
+        menu();
+    }
+
+    public void menu() {
+        //Obtenir clau
+        //Obtenir missatge
+        //Encriptar missatge cirdant el metode corresponent
+        
+        //Mostrar resultat
+    }
+}
