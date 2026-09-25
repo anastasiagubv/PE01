0976cf7252494372fe00dcf396b2ee357e0f37cf
Anastasia <anastasiagubareva28@gmail.com>
2026-09-25 17:42:03 +0200

    Fet mètode encriptar

--- ClasseCriptografica.java
+++ ClasseCriptografica.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+
 public class ClasseCriptografica {
 
-    public static String encripta(String missatge, String clau) {
-        return missatge;
+    public static String encripta(String messatge, String key) {
+        ArrayList<String> encrypted = new ArrayList<>(); 
+
+        for (int i = 0; i < messatge.length(); i++) {
+            // m = posició de cada lletra del missatge
+            char m = messatge.charAt(i);
+
+            // k = posició de cada lletra de la clau i 
+            // la llargada de la clau per poder aplicar el vigenere
+            char k = key.charAt(i % key.length());
+
+            // xor = entre els dos caràcters
+            // Agafa el unicode de cada lletra
+            // El resultat és el caràcter xifrat.
+            int xor = m ^ k;
+            encrypted.add(String.valueOf(xor));
+
+        }
+        // Concatenar 
+        // S'ajunten tots els números separats per comes
+        String encryptedMessatge = String.join(",", encrypted);
+        return encryptedMessatge;
     }
 
-    public static String desencripta(String missatge, String clau) {
-        return missatge;
+    public static String desencripta(String key, String encryptedMessatge) {
+
+        return encryptedMessatge;
     }
-}
+} 
--- programaPrincipal.java
+++ programaPrincipal.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class ProgramaPrincipal {
     Scanner sc = new Scanner(System.in);
 
+    String messatge = "";
+    String key = "";
+
     public static void main(String[] args) {
         ProgramaPrincipal s = new ProgramaPrincipal();
         s.start();
@@ -12,9 +15,31 @@ public class ProgramaPrincipal {
         menu();
     }
 
+    public String readString(String prompt) {
+        String input = "";
+        boolean valid = false;
+
+        do {
+            System.out.print(prompt);
+            try {
+                input = sc.nextLine();
+                valid = true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                valid = false;
+            }
+        } while (!valid);
+
+        return input;
+    }
+
     public void menu() {
         //Obtenir clau
+        key = readString("Introdueix clau: ");
+
         //Obtenir missatge
+        messatge = readString("Introdueix missatge: ");
+
         //Encriptar missatge cirdant el mètode d'encriptar
         //Mostrar resultat
         //Comprovar que retorna el missatge encriptat amb el mètode de desencriptar
